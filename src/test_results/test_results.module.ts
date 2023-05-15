cef62630ffefd42fedda2fa67ad894c2b5415454
Jamalov <Jamalovn07@gmail.com>
2023-05-15 21:05:05 +0500

    add full CRUD

--- src/test_results/test_results.module.ts
+++ src/test_results/test_results.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TestResultsService } from './test_results.service';
 import { TestResultsController } from './test_results.controller';
+import { TestResult } from './entities/test_result.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
+  imports:[SequelizeModule.forFeature([TestResult])],
   controllers: [TestResultsController],
   providers: [TestResultsService]
 })
--- src/test_results/test_results.service.ts
+++ src/test_results/test_results.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateTestResultDto } from './dto/create-test_result.dto';
 import { UpdateTestResultDto } from './dto/update-test_result.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { TestResult } from './entities/test_result.entity';
 
 @Injectable()
 export class TestResultsService {
-  create(createTestResultDto: CreateTestResultDto) {
-    return 'This action adds a new testResult';
+  constructor(
+    @InjectModel(TestResult) private testresultRepo: typeof TestResult,
+  ) {}
+
+  async create(createTestResultDto: CreateTestResultDto) {
+    const newTestResult = await this.testresultRepo.create(createTestResultDto);
+    return newTestResult;
   }
 
-  findAll() {
-    return `This action returns all testResults`;
+  async findAll() {
+    const testresults = await this.testresultRepo.findAll();
+    return testresults;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} testResult`;
+  async findOne(id: number) {
+    const testresult = await this.testresultRepo.findOne({ where: { id } });
+    if (!testresult) {
+      throw new BadRequestException('Test result not found');
+    }
+    return testresult;
   }
 
-  update(id: number, updateTestResultDto: UpdateTestResultDto) {
-    return `This action updates a #${id} testResult`;
+  async update(id: number, updateTestResultDto: UpdateTestResultDto) {
+    const testresult = await this.findOne(id);
+    await testresult.update(updateTestResultDto);
+    return testresult;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} testResult`;
+  async remove(id: number) {
+    const testresult = await this.findOne(id);
+    await testresult.destroy();
+    return {message:"test result deleted"}
   }
 }
